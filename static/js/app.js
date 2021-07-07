3bda7ca0878653b69c04525ff398bc9c824f4e2f
alamgituoft <81877387+alamgituoft@users.noreply.github.com>
2021-07-06 22:24:56 -0400

    Adding full code for HTML.

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,26 @@ function buildTable(data) {
     );
 });
 }
+
+// Create click function
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+}
+
+// Create if statement to see if a date was entered and filter the data to that date
+if (date) {
+    // Apply 'filter' to the table data to only keep rows where 'datetime' value matches the filter
+    filteredData = filteredData.filter(row => row.datetime === date);
+};
+
+// Rebuild the table using the filtered data
+// @NOTE: If no date was entered, then filteredData will be the original tableData
+buildTable(filteredData);
+}
+
+// Attach an event to list for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
